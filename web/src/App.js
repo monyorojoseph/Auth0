d70f36103dfccac86e4d74d818efccfa4e5aa85d
monyorojoseph <monyorojoseph@gmail.com>
2022-09-01 16:45:58 +0300

    Initial setup(React)

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
